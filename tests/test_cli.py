c321cef94dc83e3f46910bde6ba8ed141eb5f2d0
Arkadiusz Ryś <Arkadiusz.Michal.Rys@gmail.com>
2024-12-03 12:21:50 +0100

    Write cli transformation test stub

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -13,19 +13,28 @@ def test_cli_launch():
 def test_cli_package():
     """"""
     runner = CliRunner()
-    result = runner.invoke(cli, ["package"])
+    command = "package"
+    result = runner.invoke(cli, [command])
     assert result.exit_code == 0  # TODO This should not be 0
 
-    result = runner.invoke(cli, ["package", "--check"])
+    result = runner.invoke(cli, [command, "--check"])
     assert result.exit_code == 0  # TODO This should not be 0
     assert "FAIL" in result.output
 
-    result = runner.invoke(cli, ["package", "--verbose"])
+    result = runner.invoke(cli, [command, "--verbose"])
     assert result.exit_code == 0  # TODO This should not be 0
     assert "Checking" in result.output
 
-    package_name = "package"
+    package_name = command
     with TemporaryPath(package_name):
-        result = runner.invoke(cli, ["package", "--verbose", "--create", "--name", package_name])
+        result = runner.invoke(cli, [command, "--verbose", "--create", "--name", package_name])
         assert result.exit_code == 0
         assert "package created" in result.output
+
+def test_cli_transformation():
+    """"""
+    command = "transformation"
+    runner = CliRunner()
+    # TODO The roboarch2aadl transformation is not implemented at the moment
+    result = runner.invoke(cli, [command, "--roboarch2aadl"])
+    assert result.exit_code == 0  # TODO This should not be 0
